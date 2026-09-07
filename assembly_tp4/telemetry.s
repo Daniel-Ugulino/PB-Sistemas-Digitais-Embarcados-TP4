2e33cda7cfab0ee8dc90740ceff0eadb5b004516
Daniel-Ugulino <danielugulino46@gmail.com>
2026-09-06 21:19:16 -0300

    adjust

--- assembly_tp4/telemetry.s
+++ assembly_tp4/telemetry.s
@@ -3,7 +3,8 @@
 //   STX | 0x20 | speed | dist_e | dist_c | dist_d | vel_e | vel_c | vel_d | dir | ETX
 //
 //
-// Cada quadro valido entra no filter.s. Se o cru mudar, grava em spi_log.txt:
+// Cada quadro valido entra no filter.s. Se o cru mudar e alguma distancia
+// (e/c/d) for menor que cfg_dist_free, grava em spi_log.txt:
 //   Log - <cru>
 //   AVG <media/moda>
 
@@ -14,6 +15,7 @@
 .equ TEL_PAY,  8
 
 .extern filt_push
+.extern cfg_dist_free
 
 .section .bss
 .align 8
@@ -87,6 +89,18 @@ tel_save:
     b       tel_save
 
 tel_saved:
+    ldr     x0, =cfg_dist_free
+    ldrb    w1, [x0]
+    ldrb    w0, [x9, #3]
+    cmp     w0, w1
+    b.lo    tel_log
+    ldrb    w0, [x9, #4]
+    cmp     w0, w1
+    b.lo    tel_log
+    ldrb    w0, [x9, #5]
+    cmp     w0, w1
+    b.hs    tel_ok
+tel_log:
     mov     x0, x9
     bl      log_telemetry
 
